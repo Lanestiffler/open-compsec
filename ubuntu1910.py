2d6c7dfa13b2a10811b01c6e1c3d7c9208996d3b
Lanestiffler <44978369+Lanestiffler@users.noreply.github.com>
2019-12-06 08:31:28 -0500

    Create ubuntu1910.py

---
+++ ubuntu1910.py
@@ -0,0 +1,16 @@
+import os
+import time 
+
+print ("open compsec ubuntu 19.10 edition")
+time wait(3)
+print ("welcome to compsec 1.0 made by n7co")
+print ("1.run compsec")
+print ("2.quit")
+ch = raw_input("please select a option")
+if ch == '1'
+	os.system("sudo apt-get update")
+	os.system("sudo apt-get upgrade")
+	os.system("sudo apt-get install rkhunter")
+	os.system("sudo rkhunter --update")
+	os.system("sudo rkhunter --check")
+	print("installed and ran rkhunter")
